5918e840527c96ea80257a70ca97e9de0715c92a
Erick <e.martinez14@ufromail.cl>
2023-06-02 15:01:46 -0400

    import bootstap

---
+++ src/components/DetallesProducto.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+
+</div>  
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name:"DetallesProducto",
+    data(){
+        return{
+            s:"sad"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
